a6668e587b10aaa4a3d3af226ca208ff2c70960f
yanliu <87853872+liuyan133@users.noreply.github.com>
2022-04-20 23:23:30 +0800

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h2>中国时间</h2>
+		<button @click="Time">北京时间:<label>{{time}}</label></button>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+	export default {
+		// eslint-disable-next-line vue/multi-word-component-names
+		name:'About',
+		data() {
+		
+			return {
+				keyWord:'',
+				time:this.time
+			}
+		},
+		methods: {
+			Time(){
+				//请求前更新List的数据
+				
+				axios.get(`https://api.tianapi.com/worldtime/index?key=ed9f3dfa32e378106876b410f5a45227&city=%E5%8C%97%E4%BA%AC`).then(
+					response => {
+						console.log('请求成功了',response.data)
+						this.time = response.data.newslist[0].strtime
+						//请求成功后更新List的数据
+						// this.$bus.$emit('updateListData',{isLoading:false,errMsg:'',users:response.data.items})
+					},
+					error => {
+						//请求后更新List的数据
+						this.$bus.$emit('updateListData',{isLoading:false,errMsg:error.message,users:[]})
+					}
+				)
+			}
+		},
+	}
+</script>
