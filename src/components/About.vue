d7b03dbb942ea1623cd1144b286119590b44d6b9
yanliu <87853872+liuyan133@users.noreply.github.com>
2022-09-20 21:33:54 +0800

    Update About.vue
    test3.0

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>https://github.com/liuyan133/test1/blob/main/src/components/About.vue
 	<div>
-		<h2>中国时间2.0</h2>
+		<h2>中国时间3.0</h2>
 		<button @click="Time">北京时间:<label>{{time}}</label></button>
 	</div>
 </template>
